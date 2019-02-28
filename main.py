64bfc5650d77e72eae360ccf5b73208b308adcd2
ruben <ruben.ros14@gmail.com>
2019-02-27 17:46:26 -0800

    initial commit

--- main.py
+++ main.py
@@ -2,77 +2,63 @@ from helper import *
 from solution import *
 from matplotlib import pyplot as plt
 
-
-
 #Use for testing the training and testing processes of a model
-# def train_test_a_model(modelname, train_data, train_label, test_data, test_label, max_iter, learning_rate):
+def train_test_a_model(modelname, train_data, train_label, test_data, test_label, max_iter, learning_rate):
 # '''
 # you should try various number of max_iter and learning_rate
 # '''
+	print("Model: ", modelname)
+	print("Max Iterations:", max_iter)
+	print("Learning Rate: ", learning_rate)
+	
+	w = logistic_regression(train_data, train_label, max_iter, learning_rate)
+	# trainAcc = accuracy(train_data, train_label, w)
+	testAcc = accuracy(test_data, test_label, w)
 
+	# print("Training Set Accuracy:", trainAcc)
+	print("Test Set Accuracy:", testAcc)
+	print()
+	return
 
-# def test_logistic_regression():
-
+def test_logistic_regression():
 # '''
 # you should try various number of max_iter and learning_rcd ate
 # '''
+	trainFile = "../data/train.txt"
+	testFile = "../data/test.txt"
 
+	#Load Features
+	train = load_features(trainFile)
+	test = load_features(testFile)
 
+	#Split data and labels into variables
+	trainData, trainLabel = train
+	testData, testLabel = test
+
+	for idx in range(len(iterationsAndLearningRate)):
+		train_test_a_model("Logistic Regression", trainData, trainLabel, testData, testLabel, iterationsAndLearningRate[idx][0], iterationsAndLearningRate[idx][1])
+def test_thirdorder_logistic_regression():
+	trainFile = "../data/train.txt"
+	testFile = "../data/test.txt"
+
+	#Load Features
+	train = load_features(trainFile)
+	test = load_features(testFile)
+
+	#Split data and labels into variables
+	trainData, trainLabel = train
+	testData, testLabel = test
+
+	#Obtain third order of data
+	trainData = thirdorder(trainData)
+	testData = thirdorder(testData)
+
+	for idx in range(len(iterationsAndLearningRate)):
+		train_test_a_model("Third Order", trainData, trainLabel, testData, testLabel, iterationsAndLearningRate[idx][0], iterationsAndLearningRate[idx][1])
 
 if __name__ == '__main__':
-	# test_logistic_regression()
-	# test_thirdorder_logistic_regression()
-	files = "../data/train.txt"
-	train = load_features(files)
-	# max_iter = 10000
-	# learning_rate = .001
-	max_iter = 10000
-	learning_rate = .01
-	thirdOrder = thirdorder(train[0])
 
-	# thirdOrder = train[0]
+	iterationsAndLearningRate = [[10000, .1], [100000, .1], [1000, .001], [1000, .0001]]
 
-	w = logistic_regression(thirdOrder, train[1], max_iter, learning_rate)
-	acc = accuracy(thirdOrder, train[1], w)
-
-	# max_iter = 10000
-	# learning_rate = .0000001
-	# print(train[0].shape)
-	# print(thirdorder(train[0]))
-	# thirdOrder = thirdorder(train[0])
-	# thirdOrder = train[0]
-	testFile = "../data/test.txt"
-	test = load_features(testFile)
-	thirdOrder = thirdorder(test[0])
-
-	w = logistic_regression(thirdOrder, test[1], max_iter, learning_rate)
-	acc = accuracy(thirdOrder, test[1], w)
-
-	
-	# predictions = w.predict(test[0])
-	# score = w.score(test[0], test[1])
-	# print(predictions, score)
-
-	# print('accuracy', acc )
-	# testFile = "../data/test.txt"
-	# test = load_features(testFile)
-	# print(accuracy((test[0]),test[1], None))
-	# k = np.hstack(
-	# 	(np.ones(
-	# 		(train[0].shape[0], 1)
-	# 		),
-    #                              train[0]))
-	# v =np.ones(
-	# 		(train[0].shape[0], 1)
-	# 		),
-	# print(k.shape, v[0].shape)
-	# final_scores = np.dot(np.hstack(
-	# 	(np.ones(
-	# 		(train[0].shape[0], 1)
-	# 		),
-    #                              train[0])), w)
-	# print(final_scores)
-	# plt.plot(train[1],,'.')
-	# plt.show()
-
-
+	test_logistic_regression()
+	test_thirdorder_logistic_regression()
--- solution.py
+++ solution.py
@@ -1,29 +1,15 @@
+#Ruben Rosales
+#018798973
+#CECS 551
+#02-27-2019
+
 import numpy as np 
 from helper import *
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import PolynomialFeatures
 
 '''
 Homework2: logistic regression classifier
 '''
-def deriv(x):
-    
-    '''
-    Description: This function takes in a value of x and returns its derivative based on the 
-    initial function we specified.
-    
-    Arguments:
-    
-    x - a numerical value of x 
-    
-    Returns:
-    
-    x_deriv - a numerical value of the derivative of x
-    
-    '''
-    
-    x_deriv = 3* (x**2) - (6 * (x))
-    return x_deriv
+
 def logistic_regression(data, label, max_iter, learning_rate):
 	'''
 	The logistic regression classifier function.
@@ -39,34 +25,18 @@ def logistic_regression(data, label, max_iter, learning_rate):
 	Returns:
 		w: the seperater with shape (3, 1). You must initilize it with w = np.zeros((d,1))
 	'''
-	# logisticRegr = LogisticRegression()
-	# logisticRegr.fit(data, label)
 
-	# return logisticRegr
-	n = data.shape[1]
-	w = np.zeros(n)
-	# w = np.zeros((n,1))
+	n, d = data.shape
+	w = np.zeros((d,1))
 	x = data
 	y = label.reshape(-1,1)
-	# f = np.dot(data, label)
+
 	for _ in range(max_iter):
 		numerator = x * y
 		h = sigmoid(numerator * w.T)
-		gradient = np.sum(numerator/h,axis=0).reshape(w.shape)
-
-		# dw = np.sum(x*y/(1+np.exp(y*w.T*x)),axis=0).reshape(w.shape)
-		gradient /= -data.shape[0]
-		# z = np.dot(data, w.T)
-		# h = sigmoid(z)
-
-		# ## Loss: this isnt right? 
-		# l = h - label
-
-		# # loss = np.dot(h, label)
-		# gradient = (-1 / label.shape[0]) * np.dot(data.T, l)
-		w +=  -(learning_rate * gradient)
-
-	print('weights', w)
+		gradient = (-1/n) * np.sum(numerator / h, axis=0)
+		w +=  -(learning_rate * gradient.reshape((d,1)))
+		
 	return w
 
 def thirdorder(data):
@@ -84,83 +54,52 @@ def thirdorder(data):
 		The first dimension represents total samples (training: 1561; testing: 424) 
 		and the second dimesion represents total features.
 	'''
-
-	
-	# print(sq, cube)
-	# for item in data:
-	# 	sq = np.square(item[1:])
-	# 	cube = item[1:] ** 3
-	# 	print(sq, cube)
-	# polynomialFeatures= np.array()
-	# t = data[0][1:]
-	# z.append(t)
-	# t = data[1][1:]
-	# z.append(t)
-
-	# z=[]
 	polynomialFeatures = []
-	for item in data:
+	for item in data: #Example with features a & b
+		singularMult = item[1] * item[2] # a * b
+		sq = np.square(item[1:]) #a^2, b^2 
+		cube = item[1:] ** 3 #a^3, b^3
+		sqMult = np.multiply(sq,item[:0:-1]) #a^2 * b, b^2 * a
 		
-		# z.append(item[1:])
+		#merge values into an array
+		item = np.append(item, [sq[0], singularMult, sq[1], cube[0], sqMult[0], sqMult[1], cube[1]])
 
-		sq = np.square(item[1:])
-		cube = item[1:] ** 3
-		sqMult = np.multiply(sq,item[:0:-1])
-		singularMult = item[1] * item[2]
-
-		item = np.append(item, [singularMult])
-		item = np.append(item, sq)
-		item = np.append(item, cube)
-		item = np.append(item, sqMult)
 		polynomialFeatures.append(item)
 
 	polynomialFeatures = np.asarray(polynomialFeatures)
 
-	# transformer = PolynomialFeatures(degree=3)
-	# X = transformer.fit_transform(z)
-
-	# print('d',z)
-	# print('x',X)
 	return polynomialFeatures
-	# z=np.polyfit(data,data,3,full=True)
-	# print(z.shape)
-	# X_poly = np.zeros((data.shape[0], 3))
-	# print(X_poly)
-	# print(data.squeeze())
-	# print(X_poly[:, 0])
-	# # The first column in our transformed matrix is just the vector we started with.
-	# X_poly[:, 0] = data.squeeze()
-	# Cleverness Alert:
-	# We create the subsequent columns by multiplying the most recently created column
-	# by X.  This creates the sequence X -> X^2 -> X^3 -> etc...
-	# for i in range(1, 3):
-	# 	X_poly[:, i] = X_poly[:, i-1] * data.squeeze()
-	# return X_poly
 
 
 def accuracy(x, y, w):
-	z = np.dot(x, w)
-	h = sigmoid(z)
-	pred_value = np.where( h > .5, 1., -1.) 
-	acc = np.sum(y == pred_value)/y.shape[0]
-	print(acc)
-	return acc
-	# print(x[0])
-	# print(h[100:130])
-	# print(y[100:130])
-	# for item in h:
-	# 	print
-
-	# print("pred_value",pred_value)
+	'''
+	This function is used to compute accuracy of a logsitic regression model.
 	
+	Args:
+	x: input data with shape (n, d), where n represents total data samples and d represents
+		total feature numbers of a certain data sample.
+	y: corresponding label of x with shape(n, 1), where n represents total data samples.
+	w: the seperator learnt from logistic regression function with shape (d, 1),
+		where d represents total feature numbers of a certain data sample.
 
-	# # preds = np.round(sigmoid(z))
+	Return 
+		accuracy: total percents of correctly classified samples. Set the threshold as 0.5,
+		which means, if the predicted probability > 0.5, classify as 1; Otherwise, classify as -1.
+	'''
+	n = x.shape[0]
+	acc = 0
+	correct = 0
+	for i in range(n):
+		numerator = np.dot(x[i], w)
+		h = sigmoid(numerator)
+		pred = 1 if h > .5 else -1
+		
+		if y[i] == pred:
+			correct += 1
 
+	acc  = (correct/y.shape[0]) * 100
 
-	# print('p',h)
-	# print('y',y)
-	# return acc
-
+	return acc
 
 def sigmoid(z):
 	return 1 / (1 + np.exp(-z))
